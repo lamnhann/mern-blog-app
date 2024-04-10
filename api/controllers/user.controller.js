1bb87ed07bf4cb5d74db391c5a619a907f106840
THIENNHAN <93808313+lamnhann@users.noreply.github.com>
2024-04-10 15:49:10 +0700

    show users to the admin dashboard

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -80,7 +80,7 @@ export const getUsers = async (req, res, next) => {
     const sortDirection = req.query.sort === 'asc' ? 1 : -1;
 
     const users = await User.find()
-      .sort({ createAd: sortDirection })
+      .sort({ createdAt: sortDirection })
       .skip(startIndex)
       .limit(limit);
 
@@ -99,7 +99,7 @@ export const getUsers = async (req, res, next) => {
         now.getDate()
       );
       const lastMonthUsers = await User.countDocuments({
-        createAd: {$gte: oneMonthAgo},
+        createdAt: {$gte: oneMonthAgo},
       });
 
       res.status(200).json({
